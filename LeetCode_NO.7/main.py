a386c7b07adfaf511c21d06dc1d0880a62d9491c
ZZJzzj228 <273419137@qq.com>
2021-09-29 10:26:08 +0800

    first commit

---
+++ LeetCode_NO.7/main.py
@@ -0,0 +1,24 @@
+"""
+LeetCode NO.7
+给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+"""
+
+a = input("请输入一个整数")
+x = list(str(a))
+
+if x[0] == "-":
+    x.remove("-")
+    x.reverse()
+    r = ''.join(x)
+    r = - int(r)
+
+else:
+    x.reverse()
+    r = ''.join(x)
+    r = int(r)
+
+if not -pow(2, 31) <= r <= pow(2, 31) - 1:
+    r = 0
+
+print(r)
